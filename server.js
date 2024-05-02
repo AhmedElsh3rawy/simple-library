1f762f59124fd51b82743558bdaa5db168501157
Ahmed <ahmedelsharawy117@gmail.com>
2024-05-02 10:58:38 +0300

    init

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import "dotenv/config";
+import bookRoutes from "./routes/bookRoutes.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use("/api/v1/books", bookRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from the server");
