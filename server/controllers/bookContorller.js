e1078d5504855ccdadcefe97eec3a780283eb9d9
Ahmed <ahmedelsharawy117@gmail.com>
2024-05-03 06:33:50 +0300

    init

--- server/controllers/bookContorller.js
+++ server/controllers/bookContorller.js
@@ -1,14 +1,14 @@
 import pool from "../database/db.js";
-import { get_books, get_book_by_id } from "../database/bookQueries.js";
+import { get_books, get_book_by_id } from "../database/queries.js";
 
-export const getBooks = async (req, res) => {
+export const getBooks = (req, res) => {
   pool.query(get_books, async (error, results) => {
     if (error) throw error;
     await res.status(200).json(results.rows);
   });
 };
 
-export const getBookById = async (req, res) => {
+export const getBookById = (req, res) => {
   const id = parseInt(req.params.bookId);
   pool.query(get_book_by_id, [id], async (error, results) => {
     if (error) throw error;
