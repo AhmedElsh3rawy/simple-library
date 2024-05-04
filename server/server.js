f3b8568472d837631b52c0be9db5b99dc5e6c23b
Ahmed <ahmedelsharawy117@gmail.com>
2024-05-04 18:59:40 +0300

    init

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import bodyParser from "body-parser";
 import { notFound, errorHandler } from "./middlewares/errorHandler.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import customerRoutes from "./routes/customerRoutes.js";
@@ -34,8 +33,6 @@ const options = {
 const specs = swaggerJSDoc(options);
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use("/api/v1/books", bookRoutes);
